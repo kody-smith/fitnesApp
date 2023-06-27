2e384f3711af0b7988823e2b7b8196cf40c92709
Smith <kodyreedsmith@byui.edu>
2023-06-27 15:23:28 -0600

    Up to date with spreadsheet as of 6.27.23

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def main():
     ### Ask User for user info
     print("Please enter the required information below:")
     name = input("Name: ")
-    age = input("Age: ")
-    height = input("Height in inches: ")
-    weight = input("Weight in lbs: ")
-    goal = input("What is your fitness goal? \n 1. Lose Fat \n 2. Gain Muscle \n 3. Gain Muscle and Lose Fat")
-    activity_level = input("What is your activity level? \n 0 - Sedentary (No Exersize) \n 1 - Occasional (Exercise once every 2 weeks for approx 30 min. \n 2 - Light (Exercise once a week for approx 30 min \n 3 - Average (Exercise 2-3 times a week for approx 1-3 hours \n 4 - Daily (Exercise daily for approx 4-7 hrs \n 5 - Advanced (Intense daily exercise for approx 7-14 hrs")
-    time_commitment = input("How much time can you commit to working out each week? \n 1. 3 Days/week \n 2. 4 Days/week \n 3. 5 Days/week \n 4. 6 Days/week \n")
+    age = int(input("Age: "))
+    height = float(input("Height in inches: "))
+    weight = float(input("Weight in lbs: "))
+    goal = int(input("What is your fitness goal? \n 1. Lose Fat \n 2. Gain Muscle \n 3. Gain Muscle and Lose Fat \n"))
+    activity_level = int(input("What is your activity level? \n 0 - Sedentary (No Exersize) \n 1 - Occasional (Exercise once every 2 weeks for approx 30 min. \n 2 - Light (Exercise once a week for approx 30 min \n 3 - Average (Exercise 2-3 times a week for approx 1-3 hours \n 4 - Daily (Exercise daily for approx 4-7 hrs \n 5 - Advanced (Intense daily exercise for approx 7-14 hrs \n"))
+    time_commitment = int(input("How much time can you commit to working out each week? \n 1. 3 Days/week \n 2. 4 Days/week \n 3. 5 Days/week \n 4. 6 Days/week \n"))
     
     ### Calculate BMR
     bmr = 88.362+(13.397*(weight/2.205))+(4.799*(height*2.54))
@@ -22,37 +22,70 @@ def main():
 
     ### Change percent_bmr_change based on user input
     match goal:
-        case "1":
+        case 1:
             percent_bmr_change = .08
-            
-        case "2":
+        case 2:
             percent_bmr_change = 1.2
-        case "3":
+        case 3:
             percent_bmr_change = 1
 
     ### Change activity_level_change based on user input
     match activity_level:
-        case "0":
+        case 0:
             activity_level_change = percent_bmr_change - 0.05
-        case "1":
+        case 1:
             activity_level_change = percent_bmr_change - 0.03
-        case "2":
+        case 2:
             activity_level_change = percent_bmr_change - 0.02
-        case "3":
+        case 3:
             activity_level_change = percent_bmr_change
-        case "4":
+        case 4:
             activity_level_change = percent_bmr_change + 0.015
-        case "5":
+        case 5:
             activity_level_change = percent_bmr_change + 0.03
 
     ### Adjust user calorie target based on user data
-    calorie_target = bmr * activity_level_change
+    calorie_target = round(bmr * activity_level_change)
 
     ### Create Macro Profile
-    protein = (calorie_target * .3)/4
-    carbs = (calorie_target * .5)
+    if goal == 1 and activity_level <= 2:
+        protein_adjustment = .8
+    elif goal == 1 and activity_level >= 3:
+        protein_adjustment = 1
+    if goal == 2 and activity_level <= 2:
+        protein_adjustment = 1
+    elif goal == 2 and activity_level >= 3:
+        protein_adjustment = 1.2
+    if goal == 3 and activity_level <= 2:
+        protein_adjustment = .9
+    elif goal == 3 and activity_level >= 3:
+        protein_adjustment = 1.1
 
+    ##### Do math to find what carbs and protein should be based off of the percentage protein in relation to total calories
+    total_protein = (weight*protein_adjustment)*4
+    protein_percent = total_protein/calorie_target
+    remaining_percent = 1-protein_percent
+    carb_adjustment= remaining_percent * .75
+    fat_adjustment= remaining_percent * .25
 
+    #### Macro Profile
+    protein = round(weight*protein_adjustment)
+    carbs = round((carb_adjustment*calorie_target)/4)
+    fats = round((fat_adjustment*calorie_target)/9)
+
+    #### Check that all macros add up to 1
+    total_adjustment = protein_percent + carb_adjustment + fat_adjustment
+
+    ### Get Program Type
+    match time_commitment:
+        case 1:
+            program = "Full Body Program"
+        case 2:
+            program = "Upper/Lower"
+        case 3:
+            program = "PPL (5 days)"
+        case 4:
+            program = "PPL (6 days)"
 
 if __name__ == '__main__':
     main()
